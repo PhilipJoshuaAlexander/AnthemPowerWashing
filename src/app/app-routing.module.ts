cfdb980d0c5b27ef9274add7194b798507c61810
PhilipJoshuaAlexander <philip.joshua.alexander@gmail.com>
2023-05-28 17:10:17 -0700

    Begin Creating Angular Site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CommercialComponent } from './commercial/commercial.component';
+import { ContactComponent } from './contact/contact.component';
+import { EstimatesComponent } from './estimates/estimates.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { ResidentialComponent } from './residential/residential.component';
+
+
+
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'commercial', component: CommercialComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'estimates', component: EstimatesComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'residential', component: ResidentialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResidentialComponent } from './residential/residential.component';
+import { CommercialComponent } from './commercial/commercial.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EstimatesComponent } from './estimates/estimates.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResidentialComponent,
+    CommercialComponent,
+    GalleryComponent,
+    EstimatesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
